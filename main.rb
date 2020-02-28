11dac3188075c2236ad8eb9f411942244127b5d3
Leonardo Da Silva Felix <PUCMINAS\1048240@PPC01INFLL00128.pucminas.net>
2020-02-27 21:53:34 -0300

    primeira versao do jogo

---
+++ main.rb
@@ -0,0 +1,65 @@
+puts "Bem-vindo ao jogo de adivinhacao"
+puts "Qual e o seu nome?"
+nome = gets
+puts "\n\n\n\n"
+puts "Comecaremos o jogo para voce, " + nome
+puts "Escolhendo um numero secreto entre 0 e 200..."
+numero_secreto = 121
+puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
+
+puts"\n\n\n"
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else 
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O numero secreto e maior!"
+    else
+        puts "O numero secreto e menor!"
+    end 
+end 
+
+puts "\n\n\n"
+puts "Tentativa 2"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O numero secreto e maior!"
+    else 
+        puts"O numero secreto e menor!"
+    end 
+end
+
+puts "\n\n\n"
+puts "Tentativa 3"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O numero secreto e maior!"
+    else 
+        puts"O numero secreto e menor!"
+    end 
+end
